fa2183546d2a7f2d2a75c5e2a79a529c50547988
Jaime Londoño <jaime.londono2@udea.edu.co>
2015-10-15 10:09:52 -0500

    api con play-scala

---
+++ app/controllers/Application.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import models.Person;
+import models.{Person,DB}
+import play.api.libs.json.Json
+
+class Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+  
+  val formPerson: Form[Person] = Form{
+      mapping(
+          "name" -> text,
+          "age" -> number
+          )(Person.apply)(Person.unapply)
+  }
+  
+  def addPerson = Action{ implicit request =>
+    val persons = formPerson.bindFromRequest.get
+    DB.save(persons)
+    Redirect(routes.Application.index)    
+  }
+  
+  def getPerson = Action{
+        val person = DB.query[Person].fetch
+        Ok(Json.toJson(person))
+  }
+
+}
